c238c0b6544acb1981167318699f6459f0516775
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2023-04-10 20:57:25 -0300

    header, footer y arregle errores

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import {Route, Switch } from 'react-router-dom';
 import Home from "../src/screens/Home/";
-import Footer from "./Components/Footer/Footer"
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+
+
 //import Buscador from "./Components/Buscador/Buscador";
 //import DetallePeli from "./Components/DetallePeli";
 
@@ -13,7 +15,6 @@ function App() {
       <Header />
       <Switch>
         <Route path="/" component={Home}/>
-        
       </Switch>
       <Footer/>
     
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,14 +4,24 @@ import './Footer.css'
 function Footer() {
     return (
         
-        <footer className="footer">  
-            <ul className="nombresequipo">
+        <footer className="formato">
+            <nav className="uk-divider-icon">
+                <div className= "tmdb">
+                    <img  src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_1-8ba2ac31f354005783fab473602c34c3f4fd207150182061e425d366e4f34596.svg" alt="TMDBLogo" />               
+                    </div>
+                
+
+                <ul className="integrantes">
+                    <li>Emilia Tarascio</li>
                     <li>Lucila Cabello</li>
                     <li>Rocco Pellegrino</li>
-                    <li>Emilia Tarascio</li>
-            </ul>
+                </ul>
+            </nav>
         </footer>
 
     );
 }
-export default Footer
+
+export default Footer;
+
+
