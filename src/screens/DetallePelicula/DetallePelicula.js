fcb05182b4daa27c6294e18e4132a16007a7db3b
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2023-04-14 18:22:04 -0300

    Continuacion detalle de serie y peliculas. Tengo el mismo error y no me deja crear archivos en img y detalleSerie

--- src/screens/DetallePelicula/DetallePelicula.js
+++ src/screens/DetallePelicula/DetallePelicula.js
@@ -1,38 +1,47 @@
 import React, { Component } from 'react'
+import './DetallePelicula.css'
 
 class DetallePelicula extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            id:this.props.match.params.id,
-            peliculaquellega:'',
-            generos: []
+        this.state = {
+            id: this.props.match.params.id,
+            peliculaquellega: '',
+            generos: [],
+            loading: true
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         fetch(`https://api.themoviedb.org/3/movie/${this.state.id}?api_key=cd01343e0629131590a07adb7eb11c98`)
-        .then(resp => resp.json())
-        .then(data => this.setState({
-            peliculaquellega:data,
-            generos: data.genres
-        }))
-        .catch()
+            .then(resp => resp.json())
+            .then(data => this.setState({
+                peliculaquellega: data,
+                generos: data.genres,
+                loading: false
+            }))
+            .catch()
     }
-    
+
     render() {
-        return(
+        return (
             <>
-                <h2 className='name'>{this.state.peliculaquellega.title}</h2> 
-                <img className='movie' src={`https://image.tmdb.org/t/p/w342/${this.state.peliculaquellega.poster_path}`} alt=""></img>
-                <p className='resumen'>Resumen: {this.state.peliculaquellega.overview}</p>
-                <h2 className='rating'>Rating: {this.state.peliculaquellega.vote_average}<span className="rating-stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span></h2>
-                <h2 className='estreno'>Fecha de estreno: {this.state.peliculaquellega.release_date}</h2>
-                <h2 className="generos-title">Géneros:</h2>
-          <div>  <ul className="generos-list">
-              {this.state.generos.map((Genero, idx) => <li key={Genero.name + idx}>{Genero.name}</li>)}
-            </ul> </div>
+                {
+                    this.state.loading ? <img src="/img/loading.gif" alt="" /> :
+                    <>
+
+                        <h2 className='name'>{this.state.peliculaquellega.title}</h2>
+                        <img className='movie' src={`https://image.tmdb.org/t/p/w342/${this.state.peliculaquellega.poster_path}`} alt=""></img>
+                        <p className='resumen'>Resumen: {this.state.peliculaquellega.overview}</p>
+                        <h2 className='rating'>Rating: {this.state.peliculaquellega.vote_average}<span className="rating-stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span></h2>
+                        <h2 className='estreno'>Fecha de estreno: {this.state.peliculaquellega.release_date}</h2>
+                        <h2 className="generos-title">Géneros:</h2>
+                        <div>  <ul className="generos-list">
+                            {this.state.generos.map((Genero, idx) => <li key={Genero.name + idx}>{Genero.name}</li>)}
+                        </ul> </div>
+                    </>
+                }
             </>
         )
-    }    
+    }
 }
 export default DetallePelicula
--- src/screens/DetalleSerie/DetalleSerie.js
+++ src/screens/DetalleSerie/DetalleSerie.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react'
+import './DetalleSerie.css'
 
 class DetalleSerie extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            id:this.props.match.params.id,
-            seriequellega:'',
-            generos: []
+        this.state = {
+            id: this.props.match.params.id,
+            seriequellega: '',
+            generos: [],
+            loading: true
         }
     }
-    componentDidMount(){
-        fetch("`https://api.themoviedb.org/3/tv/${this.state.id}?api_key=cd01343e0629131590a07adb7eb11c98")
-        .then(resp => resp.json())
-        .then(data => this.setState({
-            seriequellega:data,
-            generos: data.genres
-        }))
-        .catch()
+    componentDidMount() {
+        fetch(`https://api.themoviedb.org/3/tv/${this.state.id}?api_key=cd01343e0629131590a07adb7eb11c98`)
+            .then(resp => resp.json())
+            .then(data => this.setState({
+                seriequellega: data,
+                generos: data.genres,
+                loading: false
+            }))
+            .catch()
     }
-    
+
     render() {
-        return(
+        return (
             <>
-                <h2 className='name'>{this.state.seriequellega.name}</h2> 
-                <img className='movie' src={`https://image.tmdb.org/t/p/w342/${this.state.seriequellega.poster_path}`} alt=""></img>
-                <p className='resumen'>Resumen: {this.state.seriequellega.overview}</p>
-                <h2 className='rating'>Rating: {this.state.seriequellega.popularity}</h2>
-                <h2 className='estreno'>Fecha de estreno: {this.state.seriequellega.first_air_date}</h2>
-                <div> <ul>
-                 {this.state.generos.map((Genero, idx) => <li key={Genero.name + idx}>{Genero.name}</li>)}
-                </ul> </div>
+                {
+                    this.state.loading ? <img src="/img/loading.gif" alt="" /> :
+                    <>
+                        <h2 className='name'>{this.state.seriequellega.name}</h2>
+                        <img className='movie' src={`https://image.tmdb.org/t/p/w342/${this.state.seriequellega.poster_path}`} alt=""></img>
+                        <p className='resumen'>Resumen: {this.state.seriequellega.overview}</p>
+                        <h2 className='rating'>Rating: {this.state.seriequellega.popularity}</h2>
+                        <h2 className='estreno'>Fecha de estreno: {this.state.seriequellega.first_air_date}</h2>
+                        <div> <ul>
+                            {this.state.generos.map((Genero, idx) => <li key={Genero.name + idx}>{Genero.name}</li>)}
+                        </ul> </div>
+                    </>
+                }
             </>
         )
     }
