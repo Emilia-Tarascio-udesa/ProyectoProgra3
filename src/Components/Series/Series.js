528199dc45573536a281337c1b5178cdb854d39d
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2023-04-14 18:16:44 -0300

    Termino componentes para series y peliculas, ademas pongo el loading

--- src/Components/Series/Series.js
+++ src/Components/Series/Series.js
@@ -10,6 +10,7 @@ class Series extends Component {
         super()
         this.state = {
             seriesPopulares: [],
+            loading: true
         }
     }
 
@@ -17,18 +18,31 @@ class Series extends Component {
         //Buscamos los datos de las series mas populares
         fetch(urlpopularSeries)
             .then(res => res.json())
-            .then(data => this.setState({
-                seriesPopulares: data.results,
-            }))
+            .then(data => {
+                let listaseriesPopulares = []
+                data.results.filter((unaPelicula, idx) => {
+                    if (idx < 4) {
+                        listaseriesPopulares.push(unaPelicula)
+                    }
+                    return null
+                })
+                this.setState(
+                    {
+                        seriesPopulares: listaseriesPopulares,
+                        loading: false
+                    }
+                )
+            })
             .catch()
     }
 
     render() {
         return (
             <React.Fragment>
-                <h1 className='encabezado'>Series Populares</h1>
+                <h1 className='encabezado'>Series Populares <Link to="/ver-todas-las-series">Ver todas</Link></h1>
                 <section className='cardContainer'>
                     {
+                        this.state.loading ? <img src="/img/loading.gif" alt="" /> :
                         this.state.seriesPopulares.map((unaSerie, idx) => <SeriesCard key={unaSerie.name + idx} datosSeries={unaSerie} />)
                     }
                 </section>
