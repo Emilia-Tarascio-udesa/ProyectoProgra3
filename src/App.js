abc5f3e17353f50e587b806ab723b78219319039
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2023-04-14 18:13:08 -0300

    Actualizo App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import {Route, Switch } from 'react-router-dom';
 import Home from "./screens/Home/Home";
-import Footer from "./Components/Footer/Footer"
-import Header from "./Components/Header/Header";
-import Buscador from "./Components/Buscador/Buscador";
 import DetallePelicula from "./screens/DetallePelicula/DetallePelicula";
 import DetalleSerie from "./screens/DetalleSerie/DetalleSerie";
 import SearchResults from "./screens/SearchResults/SearchResults"
+import VerTodasPeliculas from "./screens/VerTodas/VerTodasPeliculas";
+import VerTodasSeries from "./screens/VerTodas/VerTodasSeries";
 
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" component={Home}/>
+        <Route path="/" exact component={Home}/>
+        <Route path="/ver-todas-las-peliculas" component={VerTodasPeliculas} />
+        <Route path="/ver-todas-las-series" component={VerTodasSeries} />
         <Route path="/peliculas/detalle/id/:id" component={DetallePelicula}/>
         <Route path="/series/detalle/id/:id" component={DetalleSerie}/>
         <Route path="/resultados/title/:title" component={SearchResults}/>
